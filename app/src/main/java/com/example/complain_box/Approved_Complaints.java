fad515771601d1e1ee9ad9c5d0d24e5ad7690598
Your Name <raghavpartani1@gmail.com>
2021-03-25 11:07:16 +0530

    Updated

---
+++ app/src/main/java/com/example/complain_box/Approved_Complaints.java
@@ -0,0 +1,14 @@
+package com.example.complain_box;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Approved_Complaints extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approved__complaints);
+    }
+}
